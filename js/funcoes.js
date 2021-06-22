3530f75ee3aabd4adb8d2cd64b594211df18dca0
KayoRBC <80972278+KayoRBC@users.noreply.github.com>
2021-06-21 21:25:58 -0300

    Add files via upload

---
+++ js/funcoes.js
@@ -0,0 +1,14 @@
+function experienciaCriativa(){
+    
+    window.location.href = "paginas/experienciaCriativa.html";
+}
+
+function descEvolComputacao(){
+
+    window.location.href = "paginas/descEvolComputacao.html";
+}
+
+function raciocinioAlgoritmico(){
+
+    window.location.href = "paginas/raciocinioAlgoritmico.html";
+}
